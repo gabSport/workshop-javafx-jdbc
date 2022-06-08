e72609d69f2594e11d8a0b333c0030659c5e6e5a
Gabriel Ribeiro <gabriel_sport@hotmail.com>
2022-06-08 19:11:42 -0300

    DepartmentForm (dialog) design

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,18 +1,27 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import gui.util.Alerts;
+import gui.util.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Department;
 import model.services.DepartmentService;
@@ -38,8 +47,9 @@ public class DepartmentListController implements Initializable {
 	
 	
 	@FXML
-	public void onBtNewAction() {
-		System.out.println("onBtNewAction");
+	public void onBtNewAction(ActionEvent event) {
+		Stage parentStage = Utils.currentStage(event);
+		createDialogForm("/gui/DepartmentForm.fxml", parentStage);
 	}
 	
 	public void SetDepartmentService(DepartmentService service) {
@@ -66,5 +76,23 @@ public class DepartmentListController implements Initializable {
 		List<Department> list = service.findAll(); // aqui recupera os departamentos la do DepartmentService (que estao MOCKADOS - 'ficticios')
 		obsList = FXCollections.observableArrayList(list); // instanciei o obsList pegando os dados da list
 		tableViewDepartment.setItems(obsList); // aqui carrego os itens da tableView e mostro na tela.
+	}
+	
+	private void createDialogForm(String absolutName, Stage parentStage) { // metodo pra abrir a janela de formulario e preencher um novo Department:
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absolutName));
+			Pane pane = loader.load(); // carreguei minha view;
+			
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Enter Department data:"); // para configurar o titulo da janela;
+			dialogStage.setScene(new Scene(pane));
+			dialogStage.setResizable(false); // para a janela ser ou nao redimencionada
+			dialogStage.initOwner(parentStage); // o stage pai dessa janela;
+			dialogStage.initModality(Modality.WINDOW_MODAL); // aqui diz se a janela vai ser modal ou se terá outro comportamento;
+			dialogStage.showAndWait();
+		}
+		catch (IOException e) {
+			Alerts.showAlert("IO Exception", "Error loading view" , e.getMessage(), AlertType.ERROR);
+		}
 	}
 }

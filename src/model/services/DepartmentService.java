6a28e0b6122b81e42eeed05e3ac4222db154d80c
Gabriel Ribeiro <gabriel_sport@hotmail.com>
2022-06-08 18:27:06 -0300

    Adding database access

--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -1,18 +1,17 @@
 package model.services;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.entities.Department;
 
 public class DepartmentService { // Servico relacionado a operacoes com o Department
 // tenho que declarar uma dependencia desse servico la no controlador do DepartmentList, para carregar os elementos inseridos e mostrar na tela TableView
 	
+	private DepartmentDao dao = DaoFactory.createDepartmentDao();
+	
 	public List<Department> findAll() {
-		List<Department> list = new ArrayList<>();
-		list.add(new Department(1, "Books"));
-		list.add(new Department(2, "Computers"));
-		list.add(new Department(3, "Electronics"));
-		return list;
+		return dao.findAll(); // isso vai la no banco de dados e busca todos o departamentos.
 	}
 }
